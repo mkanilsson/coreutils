d964748cde2e31fe43631a9394ddfe64d8ef394e
Marcus Nilsson <marcus.nilsson@genarp.com>
2024-02-10 18:17:03 +0100

    true: Add true

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const programs = [_][]const u8{ "head", "yes", "false" };
+const programs = [_][]const u8{ "head", "yes", "false", "true" };
 
 pub fn build(b: *std.Build) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
